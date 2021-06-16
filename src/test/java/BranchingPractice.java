8a90fd2456728bbfaef287ab4520de5444e74b48
IrynaOlah <irynaolah@gmail.com>
2021-06-15 19:08:05 -0500

    things I have done so far

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,9 @@
 public class BranchingPractice {
     public static void main(String[] args) {
-
+        // created new project
+        // enabled version control
+        // added .gitignore with .idea, target and *.iml file
+        // made initial commit by keeping track of all files
+        // shared project on github
     }
 }
